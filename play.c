248f0a7c20ade6168aa954ba73ab0dfeaa4f13c3
Niranjan <75774547+usr-canis@users.noreply.github.com>
2023-03-03 02:48:53 +0530

    Create play.c

---
+++ play.c
@@ -0,0 +1,106 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+int i = 0;
+
+typedef struct node
+{
+    char song_name[100];
+    struct node *prev;
+    struct node *next;
+} node;
+
+node *head = NULL;
+node *first = NULL;
+node *tail = NULL;
+node *temp = NULL;
+
+node *current = NULL;
+
+void addnode(char song[])
+{
+
+    node *ptr = (node *)malloc(sizeof(node));
+    strcpy(ptr->song_name, song);
+
+    ptr->next = NULL;
+    ptr->prev = NULL;
+
+    if (head == NULL)
+    {
+        head = ptr;
+        first = head;
+        tail = head;
+    }
+    else
+    {
+        temp = ptr;
+        first->next = temp;
+        temp->prev = first;
+        first = temp;
+        tail = temp;
+    }
+
+    i++;
+}
+
+void play()
+{
+    current = head;
+    while (current != NULL)
+    {
+        char command[200];
+        sprintf(command, "start /min %s", current->song_name);
+
+        int status = system(command);
+
+        if (status != 0)
+        {
+            printf("Error playing song: %s\n", current->song_name);
+            continue;
+        }
+
+        printf("Now playing: %s\n", current->song_name);
+        current = current->next;
+    }
+}
+
+void show_mp3_files()
+{
+    printf("avaialble songs here are\n");
+    system("dir /b *.mp3");
+}
+
+int main()
+{
+    char song_name[100];
+    int choice;
+
+    while (1)
+    {
+        printf("enter your choice:\n1.display all songs\n2.add songs to playlist\n3.start playing\n4.play next song\n5.play previous song\n6.exit and kill player\n");
+        scanf("%d/n", &choice);
+        switch (choice)
+        {
+        case 1:
+            show_mp3_files();
+            break;
+
+        case 2:
+            while (1)
+            {
+                printf("Enter song name to play: ");
+                scanf("%s\n", song_name);
+                addnode(song_name);
+            }
+            break;
+
+        case 3:
+            play();
+        }
+    }
+
+    return 0;
+}
